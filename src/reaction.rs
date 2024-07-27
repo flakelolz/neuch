61c2027b936a7f23138ad3fda64df6c0448df195
Michael Gonzalez <michaelgonzalez.code@gmail.com>
2024-07-27 11:17:30 -0400

    Revamp reaction system

--- src/reaction.rs
+++ src/reaction.rs
@@ -35,9 +35,7 @@ pub struct HitEvent {
 }
 
 pub fn reaction_system(world: &mut World, hit_events: &mut Vec<HitEvent>) {
-    for (id, (state, buffer, physics)) in
-        world.query_mut::<(&mut StateMachine, &mut InputBuffer, &Physics)>()
-    {
+    for (_, state) in world.query_mut::<&mut StateMachine>() {
         let reaction = &mut state.context.reaction;
         if reaction.hitstop > 0 {
             reaction.hitstop -= 1;
@@ -48,104 +46,39 @@ pub fn reaction_system(world: &mut World, hit_events: &mut Vec<HitEvent>) {
         if reaction.blockstun > 0 && reaction.hitstop == 0 {
             reaction.blockstun -= 1;
         }
+    }
 
-        for hit_event in hit_events.iter() {
-            let next = &mut state.context.ctx.next;
-            if id == hit_event.attacker {
-                if hit_event.proximity.is_some() {
-                    continue;
-                }
+    for event in hit_events.iter() {
+        let mut players = world
+            .query_many_mut::<(&mut StateMachine, &mut InputBuffer, &mut Physics), 2>([
+                event.attacker,
+                event.defender,
+            ]);
 
-                reaction.hitstop = hit_event.properties.hitstop;
-            }
-
-            if id == hit_event.defender {
-                if !backward(buffer, &physics.facing_left) {
-                    if hit_event.proximity.is_some() {
-                        continue;
-                    }
-                    // If hit
-                    reaction.hitstop = hit_event.properties.hitstop;
-                    reaction.hitstun = hit_event.properties.hitstun;
-                    reaction.knockback = hit_event.properties.knockback;
-
-                    match hit_event.properties.strength {
-                        Strength::BackSpin => {
-                            if buffer.current().down {
-                                *next = Some(Box::new(reacting::CrouchStrong));
-                            } else {
-                                *next = Some(Box::new(reacting::BackSpin));
-                            }
-                        }
-                        Strength::FrontSpin => {
-                            if buffer.current().down {
-                                *next = Some(Box::new(reacting::CrouchStrong));
-                            } else {
-                                *next = Some(Box::new(reacting::FrontSpin));
-                            }
-                        }
-                        Strength::Rising => {
-                            if buffer.current().down {
-                                *next = Some(Box::new(reacting::CrouchStrong));
-                            } else {
-                                match hit_event.height {
-                                    Height::Upper => *next = Some(Box::new(reacting::UpperRising)),
-                                    Height::Lower => *next = Some(Box::new(reacting::LowerRising)),
-                                }
-                            }
-                        }
-                        Strength::Strong => {
-                            if buffer.current().down {
-                                *next = Some(Box::new(reacting::CrouchStrong));
-                            } else {
-                                match hit_event.height {
-                                    Height::Upper => *next = Some(Box::new(reacting::UpperStrong)),
-                                    Height::Lower => *next = Some(Box::new(reacting::LowerStrong)),
-                                }
-                            }
-                        }
-                        Strength::Mid => {
-                            if buffer.current().down {
-                                *next = Some(Box::new(reacting::CrouchMid));
-                            } else {
-                                match hit_event.height {
-                                    Height::Upper => *next = Some(Box::new(reacting::UpperMid)),
-                                    Height::Lower => *next = Some(Box::new(reacting::LowerMid)),
-                                }
-                            }
-                        }
-                        Strength::Weak => {
-                            if buffer.current().down {
-                                *next = Some(Box::new(reacting::CrouchWeak));
-                            } else {
-                                match hit_event.height {
-                                    Height::Upper => *next = Some(Box::new(reacting::UpperWeak)),
-                                    Height::Lower => *next = Some(Box::new(reacting::LowerWeak)),
-                                }
-                            }
-                        }
-                    }
-
-                // Proximity block
-                } else if hit_event.proximity.is_some() {
-                    if !reaction.block() {
-                        reaction.blockstun = hit_event.properties.blockstun;
-                        if buffer.current().down {
-                            state.context.ctx.next = Some(Box::new(reacting::GrdCrouchPre));
-                        } else {
-                            state.context.ctx.next = Some(Box::new(reacting::GrdStandPre));
-                        }
-                    }
-                // Guard
-                } else {
-                    reaction.hitstop = hit_event.properties.hitstop;
-                    reaction.blockstun = hit_event.properties.blockstun;
-                    reaction.knockback = hit_event.properties.knockback;
-                    if buffer.current().down {
-                        *next = Some(Box::new(reacting::GrdCrouchEnd))
+        let (p1, p2) = players.split_at_mut(1);
+        if let Some(Ok(p1)) = p1.get_mut(0) {
+            if let Some(Ok(p2)) = p2.get_mut(0) {
+                let (a_state, _a_buffer, _a_physics) = p1;
+                let (b_state, b_buffer, b_physics) = p2;
+                if event.proximity.is_none() {
+                    a_state.context.reaction.hitstop = event.properties.hitstop;
+                    b_state.context.reaction.hitstop = event.properties.hitstop;
+                    // KNOCKBACK
+                    if b_physics.cornered {
+                        a_state.context.reaction.knockback = -event.properties.knockback;
                     } else {
-                        *next = Some(Box::new(reacting::GrdStandEnd))
+                        b_state.context.reaction.knockback = event.properties.knockback;
                     }
+
+                    // HIT
+                    {
+                        b_state.context.reaction.hitstun = event.properties.hitstun;
+                        hit_reaction_transitions(&mut b_state.context, b_buffer, event)
+                    }
+                }
+                // GUARD
+                if backward(b_buffer, &b_physics.facing_left) {
+                    guard_reaction_transitions(&mut b_state.context, b_buffer, event);
                 }
             }
         }
@@ -203,5 +136,88 @@ pub fn guard_animation(animator: &mut Animator, context: &mut Context, timeline:
             },
         };
         animator.keyframes.push(keyframe);
+    }
+}
+
+fn hit_reaction_transitions(context: &mut Context, buffer: &InputBuffer, hit_event: &HitEvent) {
+    let next = &mut context.ctx.next;
+    match hit_event.properties.strength {
+        Strength::BackSpin => {
+            if buffer.current().down {
+                *next = Some(Box::new(reacting::CrouchStrong));
+            } else {
+                *next = Some(Box::new(reacting::BackSpin));
+            }
+        }
+        Strength::FrontSpin => {
+            if buffer.current().down {
+                *next = Some(Box::new(reacting::CrouchStrong));
+            } else {
+                *next = Some(Box::new(reacting::FrontSpin));
+            }
+        }
+        Strength::Rising => {
+            if buffer.current().down {
+                *next = Some(Box::new(reacting::CrouchStrong));
+            } else {
+                match hit_event.height {
+                    Height::Upper => *next = Some(Box::new(reacting::UpperRising)),
+                    Height::Lower => *next = Some(Box::new(reacting::LowerRising)),
+                }
+            }
+        }
+        Strength::Strong => {
+            if buffer.current().down {
+                *next = Some(Box::new(reacting::CrouchStrong));
+            } else {
+                match hit_event.height {
+                    Height::Upper => *next = Some(Box::new(reacting::UpperStrong)),
+                    Height::Lower => *next = Some(Box::new(reacting::LowerStrong)),
+                }
+            }
+        }
+        Strength::Mid => {
+            if buffer.current().down {
+                *next = Some(Box::new(reacting::CrouchMid));
+            } else {
+                match hit_event.height {
+                    Height::Upper => *next = Some(Box::new(reacting::UpperMid)),
+                    Height::Lower => *next = Some(Box::new(reacting::LowerMid)),
+                }
+            }
+        }
+        Strength::Weak => {
+            if buffer.current().down {
+                *next = Some(Box::new(reacting::CrouchWeak));
+            } else {
+                match hit_event.height {
+                    Height::Upper => *next = Some(Box::new(reacting::UpperWeak)),
+                    Height::Lower => *next = Some(Box::new(reacting::LowerWeak)),
+                }
+            }
+        }
+    }
+}
+
+fn guard_reaction_transitions(context: &mut Context, buffer: &InputBuffer, hit_event: &HitEvent) {
+    let reaction = &mut context.reaction;
+    let next = &mut context.ctx.next;
+    if hit_event.proximity.is_some() {
+        if !reaction.block() {
+            reaction.blockstun = hit_event.properties.blockstun;
+            if buffer.current().down {
+                *next = Some(Box::new(reacting::GrdCrouchPre))
+            } else {
+                *next = Some(Box::new(reacting::GrdStandPre))
+            }
+        }
+    } else {
+        reaction.hitstop = hit_event.properties.hitstop;
+        reaction.blockstun = hit_event.properties.blockstun;
+        if buffer.current().down {
+            *next = Some(Box::new(reacting::GrdCrouchEnd))
+        } else {
+            *next = Some(Box::new(reacting::GrdStandEnd))
+        }
     }
 }
